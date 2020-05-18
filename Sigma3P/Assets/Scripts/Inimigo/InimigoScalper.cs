92179895c3740b407f1dc10b0c87c0a52a522793
Vitor Nardelli <vitornardelli17@gmail.com>
2020-05-18 15:40:12 -0300

    Rage mode Scalper

---
+++ Sigma3P/Assets/Scripts/Inimigo/InimigoScalper.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using CSF;
+
+public class InimigoScalper : MonoBehaviour
+{
+    public NavMeshAgent navMesh;
+    public GameObject player;
+    public float distanciaDoAtaque = 2;
+    public float distanciaDoPlayer;
+    public float velocidade = 4;
+    Animator anim;
+    public int hp = 100;
+    RagDoll ragScript;
+
+    public GameObject objDesliza;
+    public bool estaMorto;
+    public bool bravo;
+    public Renderer render;
+    public bool invencivel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+        anim = GetComponent<Animator>();
+        ragScript = GetComponent<RagDoll>();
+        render = GetComponentInChildren<Renderer>();
+
+        invencivel = false;
+        estaMorto = false;
+        ragScript.DesativaRagdoll();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!estaMorto)
+        {
+            distanciaDoPlayer = Vector3.Distance(transform.position, player.transform.position);
+            VaiAtrasJogador();
+            OlhaParaPlayer();
+
+            if(hp <= 50 && !bravo)
+            {
+                bravo = true;
+                anim.ResetTrigger("levouTiro");
+                ParaDeAndar();
+                anim.CrossFade("Zombie Scream", 0.2f);
+                render.material.color = Color.red;
+                velocidade = 8;
+                
+            }
+
+            if (hp <= 0 && !estaMorto)
+            {
+                render.material.color = Color.white;
+                objDesliza.SetActive(false);
+                estaMorto = true;
+                ParaDeAndar();
+                navMesh.isStopped = false;
+                ragScript.AtivaRagdoll();
+                
+            }
+        }
+    }
+
+    void OlhaParaPlayer()
+    {
+        Vector3 direcaoOlha = player.transform.position - transform.position;
+        Quaternion rotacao = Quaternion.LookRotation(direcaoOlha);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotacao, Time.deltaTime * 300);
+    }
+
+    void VaiAtrasJogador()
+    {
+        navMesh.speed = velocidade;
+
+        //inimigo percebe player e segue
+        //Revisar essa parte do código
+        if(distanciaDoPlayer < 10)
+        {
+        anim.SetBool("vaiParar",false);
+        anim.SetBool("paraAtaque",true);
+        anim.SetBool("podeAndar",true);
+        if(invencivel == false){
+        navMesh.isStopped = false;
+        navMesh.SetDestination(player.transform.position);
+        }
+        else
+        {
+            navMesh.isStopped = true;
+        }
+
+            if(distanciaDoPlayer <= distanciaDoAtaque)
+            {
+                navMesh.isStopped = true;
+                //Debug.Log("Atacando");
+                anim.SetTrigger("ataca");
+                anim.SetBool("podeAndar",false);
+                anim.SetBool("paraAtaque",false);
+                CorrigeRigEntra();
+            }
+        }
+        if(distanciaDoPlayer >=15)
+        {
+            anim.SetBool("podeAndar",false);
+            anim.SetBool("vaiParar",true);
+            anim.SetBool("paraAtaque",true);
+            navMesh.isStopped = true;
+        }
+        if(anim.GetBool("podeAndar"))
+        {
+            anim.SetBool("vaiParar",false);
+            anim.SetBool("paraAtaque",true);
+
+            if(invencivel == false){
+            navMesh.isStopped = false;
+            navMesh.SetDestination(player.transform.position);
+            }
+            else
+            {
+            navMesh.isStopped = true;
+            }
+
+            anim.ResetTrigger("ataca");
+            CorrigeRigSai();
+        }
+        if(anim.GetBool("vaiParar"))
+        {
+            navMesh.isStopped = true;
+            navMesh.SetDestination(player.transform.position);
+            anim.ResetTrigger("ataca");
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            CorrigeRigEntra();
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            CorrigeRigSai();
+        }
+    }
+
+    void CorrigeRigEntra()
+    {
+        ragScript.rigid.isKinematic = true;
+        ragScript.rigid.velocity = Vector3.zero;
+    }
+
+    void CorrigeRigSai()
+    {
+       ragScript.rigid.isKinematic = false;
+    }
+
+     public void LevouDano(int dano)
+    {
+        int n;
+
+        n = Random.Range(0, 10);
+
+        if(n % 2 == 0 && !bravo)
+        {
+            anim.SetTrigger("levouTiro"); 
+            ParaDeAndar();
+        }
+
+        if (!invencivel)
+        {
+            
+            hp -= dano;
+        }
+    }
+
+    void ParaDeAndar()
+    {
+        navMesh.isStopped = true;
+        anim.SetBool("podeAndar", false);
+        CorrigeRigEntra();
+    }
+
+    public void DaDanoPlayer()
+    {
+        player.GetComponent<MovimentaPersonagem>().hp -=10;
+    }
+    
+    public void FicaInvencivel()
+    {
+        invencivel = true;
+        ParaDeAndar();
+    }
+
+    public void SaiInvencivel()
+    {
+        invencivel = false;
+        anim.speed = 2;
+    }
+
+}
---
+++ Sigma3P/Assets/Scripts/Inimigo/RagDoll.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDoll : MonoBehaviour
+{
+    List<Rigidbody> ragdollRigids = new List<Rigidbody>();
+    public Rigidbody rigid;
+    List<Collider> ragdollColliders = new List<Collider>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void DesativaRagdoll()
+    {
+        Rigidbody[] rigs = GetComponentsInChildren<Rigidbody>();
+
+        for (int i = 0; i < rigs.Length; i++)
+        {
+            if(rigs[i] == rigid)
+            {
+                continue;
+            }
+
+            ragdollRigids.Add(rigs[i]);
+            rigs[i].isKinematic = true;
+
+            Collider col = rigs[i].gameObject.GetComponent<Collider>();
+            col.isTrigger = true;
+            ragdollColliders.Add(col);
+        }
+    }
+
+     public void AtivaRagdoll()
+    {
+        for (int i = 0; i < ragdollRigids.Count; i++)
+        {
+            ragdollRigids[i].isKinematic = false;
+            ragdollColliders[i].isTrigger = false;
+            ragdollRigids[i].transform.gameObject.layer = 10;
+        }
+
+        rigid.isKinematic = true;
+        GetComponent<CapsuleCollider>().enabled = false;
+        StartCoroutine("FinalizaAnimacao");
+        
+    }
+
+    IEnumerator FinalizaAnimacao()
+    {
+        yield return new WaitForEndOfFrame();
+        GetComponent<Animator>().enabled = false;
+        this.enabled = false;
+    }
+}
